7106e85af8ecbdd8307dcb517073d3cff8b32196
Alexv035 <voronkov.a.g@gmail.com>
2022-09-25 01:32:32 +0300

    Lesson_2 final

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from mainapp.views import *
 
-urlpatterns = [path("admin/", admin.site.urls), path("mainapp/", include("mainapp.urls"))]
+urlpatterns = [
+    path("", RedirectView.as_view(url='mainapp/')),
+    path("admin/", admin.site.urls),
+    path("mainapp/", include("mainapp.urls"))
+]
