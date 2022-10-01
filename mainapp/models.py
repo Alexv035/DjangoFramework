28db9f3dda1a654b3484e4085b1ea971d6cc18d7
Alexv035 <voronkov.a.g@gmail.com>
2022-10-01 19:15:09 +0300

    Lesson 4. Code style fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from ast import Delete
+
 from django.db import models
 
 
@@ -6,15 +7,9 @@ class News(models.Model):
     title = models.CharField(max_length=256, verbose_name="Title")
     preambule = models.CharField(max_length=1024, verbose_name="Preambule")
     body = models.TextField(blank=True, null=True, verbose_name="Body")
-    body_as_markdown = models.BooleanField(
-        default=False, verbose_name="As markdown"
-    )
-    created = models.DateTimeField(
-        auto_now_add=True, verbose_name="Created", editable=False
-    )
-    updated = models.DateTimeField(
-        auto_now=True, verbose_name="Edited", editable=False
-    )
+    body_as_markdown = models.BooleanField(default=False, verbose_name="As markdown")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -33,18 +28,10 @@ class CoursesManager(models.Manager):
 class Courses(models.Model):
     objects = CoursesManager()
     name = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(
-        verbose_name="Description", blank=True, null=True
-    )
-    description_as_markdown = models.BooleanField(
-        verbose_name="As markdown", default=False
-    )
-    cost = models.DecimalField(
-        max_digits=8, decimal_places=2, verbose_name="Cost", default=0
-    )
-    cover = models.CharField(
-        max_length=25, default="no_image.svg", verbose_name="Cover"
-    )
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Cost", default=0)
+    cover = models.CharField(max_length=25, default="no_image.svg", verbose_name="Cover")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
     updated = models.DateTimeField(auto_now=True, verbose_name="Edited")
     deleted = models.BooleanField(default=False)
@@ -61,18 +48,10 @@ class Lesson(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
     title = models.CharField(max_length=256, verbose_name="Name")
-    description = models.TextField(
-        verbose_name="Description", blank=True, null=True
-    )
-    description_as_markdown = models.BooleanField(
-        verbose_name="As markdown", default=False
-    )
-    created = models.DateTimeField(
-        auto_now_add=True, verbose_name="Created", editable=False
-    )
-    updated = models.DateTimeField(
-        auto_now=True, verbose_name="Edited", editable=False
-    )
+    description = models.TextField(verbose_name="Description", blank=True, null=True)
+    description_as_markdown = models.BooleanField(verbose_name="As markdown", default=False)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created", editable=False)
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edited", editable=False)
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
@@ -94,9 +73,7 @@ class CourseTeachers(models.Model):
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
-        return "{0:0>3} {1} {2}".format(
-            self.pk, self.name_second, self.name_first
-        )
+        return "{0:0>3} {1} {2}".format(self.pk, self.name_second, self.name_first)
 
     def delete(self, *args):
         self.deleted = True
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,12 @@
 import json
 from datetime import datetime
 
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
-from .models import News
 
 from mainapp import models as mainapp_models
-from django.shortcuts import get_object_or_404
+
+from .models import News
 
 # Create your views here.
 
