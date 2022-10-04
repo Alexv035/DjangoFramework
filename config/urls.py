d90772504c87c301d8841beceadc88b11644fe73
Alexv035 <voronkov.a.g@gmail.com>
2022-10-04 12:01:57 +0300

    Lesson 5. - 1

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from argparse import Namespace
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -27,3 +29,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("mainapp/", include("mainapp.urls", namespace=MainappConfig.name)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
