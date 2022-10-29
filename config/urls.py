c0031c572670cce9b07676ce4a67546a3c3e4060
Alexv035 <voronkov.a.g@gmail.com>
2022-10-29 03:20:03 +0300

    Lesson 8. 1

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+import debug_toolbar
 
 from mainapp.apps import MainappConfig
 from mainapp.views import *
@@ -27,10 +28,13 @@ from mainapp.views import *
 urlpatterns = [
     path("", RedirectView.as_view(url="mainapp/")),
     path("admin/", admin.site.urls),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("mainapp/", include("mainapp.urls", namespace=MainappConfig.name)),
     path("authapp/", include("authapp.urls", namespace="authapp")),
     path("social_auth/", include("social_django.urls", namespace="social")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,10 @@
 import json
 from datetime import datetime
 
+import logging
+from django.core.cache import cache
+from django.conf import settings
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -14,6 +18,8 @@ from mainapp import models as mainapp_models
 from .models import News
 
 # Create your views here.
+
+logger = logging.getLogger(__name__)
 
 
 class MainPageView(TemplateView):
@@ -95,11 +101,17 @@ class CoursesDetailView(TemplateView):
     template_name = "mainapp/courses_detail.html"
 
     def get_context_data(self, pk=None, **kwargs):
+        logger.debug("Yet another log message")
         context = super(CoursesDetailView, self).get_context_data(**kwargs)
-        context["course_object"] = get_object_or_404(mainapp_models.Courses, pk=pk)
-        context["lessons"] = mainapp_models.Lesson.objects.filter(course=context["course_object"])
-        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(course=context["course_object"])
-
+        context["course_object"] = get_object_or_404(
+            mainapp_models.Courses, pk=pk
+        )
+        context["lessons"] = mainapp_models.Lesson.objects.filter(
+            course=context["course_object"]
+        )
+        context["teachers"] = mainapp_models.CourseTeachers.objects.filter(
+            course=context["course_object"]
+        )
         if not self.request.user.is_anonymous:
             if not mainapp_models.CourseFeedback.objects.filter(
                 course=context["course_object"], user=self.request.user
@@ -107,10 +119,28 @@ class CoursesDetailView(TemplateView):
                 context["feedback_form"] = mainapp_forms.CourseFeedbackForm(
                     course=context["course_object"], user=self.request.user
                 )
-        context["feedback_list"] = mainapp_models.CourseFeedback.objects.filter(
-            course=context["course_object"]
-        ).order_by("-created", "-rating")[:5]
-
+                cached_feedback = cache.get(f"feedback_list_{pk}")
+        if not cached_feedback:
+            context["feedback_list"] = (
+                mainapp_models.CourseFeedback.objects.filter(
+                    course=context["course_object"]
+                )
+                .order_by("-created", "-rating")[:5]
+                .select_related()
+            )
+            cache.set(
+                f"feedback_list_{pk}", context["feedback_list"], timeout=300
+            )
+            # 5 minutes
+            # Archive object for tests --->
+            import pickle
+            with open(
+                f"mainapp/fixtures/006_feedback_list_{pk}.bin", "wb"
+            ) as outf:
+                pickle.dump(context["feedback_list"], outf)
+            # <--- Archive object for tests
+        else:
+            context["feedback_list"] = cached_feedback
         return context
 
 
@@ -120,5 +150,6 @@ class CourseFeedbackFormProcessView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         self.object = form.save()
-        rendered_card = render_to_string("mainapp/includes/feedback_card.html", context={"item": self.object})
+        rendered_card = render_to_string(
+            "mainapp/includes/feedback_card.html", context={"item": self.object})
         return JsonResponse({"card": rendered_card})
